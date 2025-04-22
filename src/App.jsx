b2947f29021dee4b0f047ed40c429be3a967adf6
Cynthia Pendo <pendoc15@gmail.com>
2025-04-22 16:02:42 +0300

    chore: scaffold project folders and set up basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomeLayout from "./layout/HomeLayout";
+import Home from "./pages/home/Home";
+
 const App = () => {
-  return <div>project init</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route  path="/" element={<HomeLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,7 @@
+const Home = () => {
+  return (
+    <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus, quas.</div>
+  )
+}
+
+export default Home
