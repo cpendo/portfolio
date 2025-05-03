146a86832b25406ad91eac874e68a1ff095960d1
Cynthia Pendo <pendoc15@gmail.com>
2025-05-03 15:47:07 +0300

    refactor: change about me section text and image

--- src/pages/home/sections/AboutMe.jsx
+++ src/pages/home/sections/AboutMe.jsx
@@ -1,31 +1,50 @@
-import AvatarImage from "../../../assets/avatar.png";
+import AvatarImage from "../../../assets/img-1.png";
 
 const AboutMe = () => {
   return (
     <div className="w-full h-screen px-6 pt-8">
-      <div className="flex flex-row justify-between items-start">
-        <div className="flex flex-2 flex-col gap-8">
-          <h3 className="font-headings uppercase text-green-500 text-6xl">
+      <div className="flex flex-row justify-between gap-10">
+        <div className="flex flex-1">
+          <img
+            src={AvatarImage}
+            alt="Avatar image"
+            className="w-[90%] p-6  rounded-xl"
+          />
+        </div>
+        
+        <div className="flex flex-1 flex-col">
+          <h3 className="font-headings uppercase text-green-500 text-4xl">
             About Me
           </h3>
-          <p className="text-2xl font-semibold">
-            I though I&apos;d become a doctor or a lawyer (yes, like everyone
-            else). Somewhere along the way, I got into coding and something
-            about it just fit.
-          </p>
-          <p className="text-2xl">
-            Sometimes it feels like free-fall. One minute I know exactly what
-            I&apos;m doing, the next, a simple bug makes me question everything.
-            Still, I enjoy the challenge. There&apos;s something satisfying
-            about solving problems and building things that just work.
-          </p>
 
-          <h4 className="font-headings text-4xl">
-            I want my work to be thoughtful, practical, and a little bit clever.
-          </h4>
-        </div>
-        <div className="flex flex-1">
-          <img src={AvatarImage} alt="Avatar image" className="" />
+          <div className="flex flex-col gap-6 pt-5 text-lg">
+            <h4 className="capitalize text-4xl font-bold">
+              Hey, I&apos;m Cynthia
+            </h4>
+            <p>
+              A software developer based in Kenya who builds apps for fun,
+              function, and when i&apos;m lucky, both. I like my work
+              thoughtful, practical, and a little bit clever.
+            </p>
+
+            <p>
+              Sometimes it feels like free-fall. One moment I know exactly what
+              I&apos;m doing, the next I&apos;m staring at a bug that makes me
+              question my life choices. But I love that. I love the challenge. I
+              love the satisfaction of getting something just right.
+            </p>
+
+            <p>
+              I&apos;m currently working on projects that sharpen both my
+              frontend and backend skills. And I&apos;m always looking to grow
+              as a developer and a problem-solver.
+            </p>
+
+            <p className="font-black italic">
+              When I&apos;m not working, I&apos;m sketching out new app ideas,
+              watching TV shows, or casually befriending AI just in case they take over and I need a character reference.
+            </p>
+          </div>
         </div>
       </div>
     </div>
